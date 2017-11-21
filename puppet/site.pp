fdc962aae58f9eeb90f4ac1779a0bc86dd281d64
jake <jake@autrui.com>
2017-11-21 16:29:53 -0500

    fixing up war path and adding packer binary

--- puppet/site.pp
+++ puppet/site.pp
@@ -1,6 +1,6 @@
 node default {
 	tomcat::war { 'jenkins.war':
-	  catalina_base => '/opt/tomcat/webapps',
+	  catalina_base => '/opt/tomcat/',
 	  war_source    => 'http://mirrors.jenkins.io/war-stable/latest/jenkins.war',
 	}
 }
